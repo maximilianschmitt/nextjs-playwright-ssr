351b3e29f6645724858f102f7159ebc77d54e7db
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2024-03-15 16:08:43 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default async function Home() {
+	const repos = await fetch('https://api.github.com/users/maximilianschmitt/repos?sort=updated')
+		.then((res) => res.json())
+		.then((repos) => repos.slice(0, 3))
+
+	return (
+		<main className="p-4">
+			<h1 className="text-xl">Latest repositories</h1>
+			<ul className="mt-4 flex flex-col gap-1">
+				{repos.map((repo: any) => {
+					return <li key={repo.id}>{repo.full_name}</li>
+				})}
+			</ul>
+		</main>
+	)
+}
